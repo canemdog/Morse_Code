a2ea618dd1b88ddcff91f6483174c1c80a38bfe1
canemdog <canemdog@gmail.com>
2016-03-10 22:39:15 +0000

    Merge branch 'Rational_Expr'
    # Conflicts:
    #	Morse_Code_Gui.py

--- Morse_Code_Gui.py
+++ Morse_Code_Gui.py
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 import string
+=======
+
+import string, re
+>>>>>>> Rational_Expr
 from tkinter import *
 from tkinter import ttk
 
@@ -10,6 +15,7 @@ Morse_Code = {"A":(0,1), "B":(1,0,0,0), "C":(1,0,1,0), "C":(1,0,1,0), "D":(1,0,0
 , "1":(0,1,1,1,1), "2":(0,0,1,1,1), "3":(0,0,0,1,1), "4":(0,0,0,0,1), "5":(0,0,0,0,0), "6":(1,0,0,0,0)
 , "7":(1,1,0,0,0), "8":(1,1,1,0,0), "9":(1,1,1,1,0), ".":(0,1,0,1,0,1), ",":(1,1,0,0,1,1), 
 ":":(1,1,1,0,0,0), "?":(0,0,1,1,0,0), "'":(0,1,1,1,1,0), "-":(1,0,0,0,0,1), "/":(1,0,0,1,0), 
+<<<<<<< HEAD
 "(":(1,0,1,1,0,1), ")":(1,0,1,1,0,1), "\"":(0,1,0,0,1,0), "@":(0,1,1,0,1,0), "=":(1,0,0,0,1)}
 
 
@@ -66,3 +72,103 @@ Decode_Entry.focus()
 root.bind('<Return>', encode)
 
 root.mainloop()
+=======
+"(":(1,0,1,1,0,1), ")":(1,0,1,1,0,1), "\"":(0,1,0,0,1,0), "@":(0,1,1,0,1,0), "=":(1,0,0,0,1), " ":()}
+
+
+
+class ConstrainedEntry(ttk.Entry):
+    def __init__(self, *args, **kwargs):
+        ttk.Entry.__init__(self, *args, **kwargs)
+        #Recursively call itself on each key stroke of the message Entry field to validate if an invalid key press is detected
+        vcmd = (self.register(self.on_validate),"%P")
+        self.configure(validate="key", validatecommand=vcmd)
+
+    def disallow(self):
+        self.bell()
+
+    def on_validate(self, message):
+        try:
+            # Check that the input string is allowed using Relational Expressions
+            # First strip it of white chars at front and end vis .strip() command
+            if message.strip() == "": return True
+            message_1 = str(message)
+            #use striped string to check if valid
+            if re.match(r"^[A-Za-z0-9\.,:\?'\-\/\(\)\\@=\s]*$", message_1) == None:
+            #Invalid input. Only charactors defined in the dictionary are allowed
+
+                self.disallow()
+                return False
+        # handle exceptions        
+        except ValueError:
+            self.disallow()
+            return False
+
+        return True
+
+    def encode(self, *args):
+        try:
+            # e_message is the Encoded Message, Post Morse code translation
+            e_message = ""
+            # nessage is the message to be encoded into Morse
+            message = str(Input_Str.get())
+
+            for char in message:
+                #Convert each character into upper case only
+                char = str.upper(char)
+                #Determien the length of the tuple to know how many dit or dah you expect to append to the string
+                tuple_len = int(len(Morse_Code[char]))
+                x = 0
+                int(x)
+                # while your position x is less than the length of the word apend the next dit or dah to make up the word in Morse
+                while x < tuple_len:
+                    e_message = e_message + str((Morse_Code[char][x]))
+                    x = x + 1
+                #Add a space after each encoded letter
+                e_message = e_message + str(" ")
+            # Ouput_Str is the Variable in Tkinter Tex field that you set and display back to the user
+            Output_Str.set (e_message)
+        except ValueError:
+            pass
+        return
+
+root = Tk()
+#Define global varialble for the clear and encoded messages
+Input_Str = StringVar()
+Output_Str = StringVar()
+
+#Define the Tkinter Frame
+content = ttk.Frame(root)
+root.title("Morse Code")
+frame = ttk.Frame(content, borderwidth=5, relief="sunken", width=200, height=100) 
+#Define the widgets that will make up the frame
+#Button for Encoding and Quitting   
+button1 = ttk.Button(content, text="Encode", command= lambda: ConstrainedEntry.encode(content, Input_Str))
+button2 = ttk.Button(content, text="Quit", command=root.destroy)
+#Defining the output of the conversion to Morese
+label1 = ttk.Label(content, text="Message:", anchor="w")
+# Calls the ContrainedEntry method to check that values entered are valid
+Encode_Entry = ConstrainedEntry(content, width=40, textvariable=Input_Str)
+label2 = ttk.Label(content, text="Morse Code:", anchor="w")
+# Result display of coded result
+Coded_Result = ttk.Entry(content, width=40, textvariable=Output_Str)
+#Construct the window
+
+#Draw the frame and the widgets that make up the GUi
+content.grid (column=0 , row=0)
+frame.grid (column=0, row=0, columnspan=2, rowspan=3)
+label1.grid(column=1, row=1, sticky=(N,W))
+Encode_Entry.grid(column=2, row=1, sticky=(N,W))
+label2.grid(column=1, row=2, sticky=(N,W))
+Coded_Result.grid(column=2, row=2, sticky=(N,W))
+button1.grid(column=3, row=3, sticky=W)
+button2.grid(column=2, row=3, sticky=W)
+
+#After drawing the Frame and its widgets set the focus on the Entry box for the message to be encoded
+Encode_Entry.focus()
+
+#Set the main loop for the program to run
+root.mainloop()
+
+
+>>>>>>> Rational_Expr
